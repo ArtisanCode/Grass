adc13b80a516f6935efe6a2ce93aaa019b3313da
TK <tom.c.kuhn@gmail.com>
2014-11-19 21:12:16 +0000

    Added test cases for Determine type

--- Test.Grass/GrassTests.cs
+++ Test.Grass/GrassTests.cs
@@ -39,10 +39,17 @@ namespace GrassTests
 
         [TestMethod]
         public void DetermineType_ValidTypesRetrieved_NamespacesUpdated()
-        {           
+        {
             DetermineTypeTest<string>("String", "System");
+            DetermineTypeTest<int>("Int32", "System");
+            DetermineTypeTest<short>("Int16", "System");
+            DetermineTypeTest<long>("Int64", "System");
+            DetermineTypeTest<byte>("Byte", "System");
+            DetermineTypeTest<decimal>("Decimal", "System");
+            DetermineTypeTest<float>("Single", "System");
             DetermineTypeTest<HashSet<string>>("HashSet<String>", "System.Collections.Generic");
             DetermineTypeTest<Tuple<string, int, long>>("Tuple<String, Int32, Int64>", "System");
+            DetermineTypeTest<List<List<List<List<List<List<string>>>>>>>("List<List<List<List<List<List<String>>>>>>", "System.Collections.Generic");
         }
 
         private void DetermineTypeTest<T>(string expectedOutput, string expectedNamespaceAdded, HashSet<string> existingNamespaces = null )
